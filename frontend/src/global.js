8186f7a92e9a43735f26267f599c1d19dbc5ee53
Genivaldo Alves <56618687+geniinho@users.noreply.github.com>
2021-10-22 15:49:50 -0300

    Update global.js

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -3,8 +3,7 @@ import Vue from 'vue'
 
 export const userKey = '__knowledge_user'
 export const baseApiUrl = 'http://localhost:3000'
-//export const baseApiUrl = 'http://189.89.155.137:3000'
-//export const baseApiUrl = 'https://b901a4cc771a.ngrok.io'
+
 
 export function showError(e) {
     if(e && e.response && e.response.data) {
